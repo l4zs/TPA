aee1ae35e8da23e84fa07e731cc85210a2d7364a
l4zs <business@l4zs.de>
2022-03-31 03:53:55 +0200

    update to 1.18.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,18 +2,16 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.10"
-    id("com.github.johnrengelman.shadow") version "7.1.2"
-    id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
     id("io.papermc.paperweight.userdev") version "1.3.5"
     id("net.minecrell.plugin-yml.bukkit") version "0.5.1"
 }
 
 group = "de.l4zs"
-version = "1.0.1"
+version = "1.1.0"
 
 repositories {
     mavenCentral()
-    mavenLocal()
 }
 
 dependencies {
@@ -22,21 +20,11 @@ dependencies {
 
     // KSpigot dependency
     implementation("net.axay", "kspigot", "1.18.2")
-
-    // Kotlinx.Coroutines dependency
-    api("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.6.0")
-    api("org.jetbrains.kotlinx", "kotlinx-coroutines-jdk8", "1.6.0")
 }
 
 tasks {
-    jar {
-        // Disabled, because we use the shadowJar task for building our jar
-        enabled = false
-    }
     build {
-        dependsOn(reobfJar, shadowJar)
-    }
-    shadowJar {
+        dependsOn(reobfJar)
     }
     withType<KotlinCompile> {
         kotlinOptions {
@@ -51,19 +39,14 @@ java {
 }
 
 bukkit {
-    name = "ATPA"
+    name = "TPA"
     apiVersion = "1.18"
     authors = listOf(
         "l4zs",
     )
     main = "$group.tpa.TPA"
     version = getVersion().toString()
-//    libraries = listOf(
-//        "net.axay:kspigot:1.18.0",
-//        "org.jetbrains.kotlin:kotlin-stdlib:1.6.10",
-//        "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0",
-//        "org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.0",
-//        "com.mojang:brigadier:1.0.18",
-//        "com.google.code.gson:gson:2.8.9",
-//    )
+    libraries = listOf(
+        "net.axay:kspigot:1.18.2",
+    )
 }
