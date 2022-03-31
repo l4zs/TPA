0cdb20c9bd2d37ba6a926ac956ec9f0ad06fbeda
l4zs <business@l4zs.de>
2022-03-31 03:54:47 +0200

    enable editing of translations

--- src/main/kotlin/de/l4zs/tpa/TPA.kt
+++ src/main/kotlin/de/l4zs/tpa/TPA.kt
@@ -8,32 +8,64 @@ import de.l4zs.tpa.command.TpahereallCommand
 import de.l4zs.tpa.command.TpcancelCommand
 import de.l4zs.tpa.command.TpdenyCommand
 import de.l4zs.tpa.command.TptoggleCommand
+import de.l4zs.tpa.config.ConfigManager
 import de.l4zs.tpa.i18n.TranslationsProvider
 import de.l4zs.tpa.listener.BackListener
+import de.l4zs.tpa.util.TpaManager
 import net.axay.kspigot.commands.register
 import net.axay.kspigot.event.register
+import net.axay.kspigot.extensions.pluginManager
 import net.axay.kspigot.main.KSpigot
+import java.util.Locale
+import kotlin.io.path.div
+import kotlin.io.path.notExists
 
 class TPA : KSpigot() {
 
-    private lateinit var translationsProvider: TranslationsProvider
+    lateinit var translationsProvider: TranslationsProvider
+    lateinit var configManager: ConfigManager
+    lateinit var tpaManager: TpaManager
 
     override fun startup() {
         BackCommand().register().register(true)
-        TpacceptCommand().register().register(true)
-        TpaCommand().register().register(true)
-        TpahereCommand().register().register(true)
-        TpahereallCommand().register().register(true)
-        TpcancelCommand().register().register(true)
-        TpdenyCommand().register().register(true)
+        TpacceptCommand().register(this).register(true)
+        TpaCommand().register(this).register(true)
+        TpahereCommand().register(this).register(true)
+        TpahereallCommand().register(this).register(true)
+        TpcancelCommand().register(this).register(true)
+        TpdenyCommand().register(this).register(true)
         TptoggleCommand().register().register(true)
 
         BackListener().register().register()
 
-        translationsProvider = TranslationsProvider()
-        translationsProvider.registerTranslations()
+        val translationNames = listOf("general_en.properties", "general_de.properties")
+        // save default translations
+        translationNames.forEach {
+            if ((dataFolder.toPath() / "translations" / it).notExists()) {
+                saveResource("translations/$it", false)
+            }
+        }
+
+        configManager = ConfigManager(this)
+        translationsProvider = TranslationsProvider(this)
+        reloadTranslations()
+        tpaManager = TpaManager(this)
     }
 
     override fun shutdown() {
+    }
+
+    fun reloadTranslations() {
+        val locales = configManager.config.yml.getStringList("translations").map { Locale(it) }
+
+        if (locales.isEmpty()) {
+            logger.severe("No translations found. Please add at least one translation (default available are 'en' and 'de') to the config.yml")
+            pluginManager.disablePlugin(this)
+        }
+
+        translationsProvider.unregisterTranslations(locales.first())
+        translationsProvider.locales.clear()
+        translationsProvider.locales.addAll(locales)
+        translationsProvider.registerTranslations()
     }
 }
--- src/main/kotlin/de/l4zs/tpa/i18n/TranslationsProvider.kt
+++ src/main/kotlin/de/l4zs/tpa/i18n/TranslationsProvider.kt
@@ -1,39 +1,54 @@
 package de.l4zs.tpa.i18n
 
+import de.l4zs.tpa.TPA
 import net.kyori.adventure.key.Key
 import net.kyori.adventure.translation.GlobalTranslator
 import net.kyori.adventure.translation.TranslationRegistry
+import java.io.File
+import java.net.MalformedURLException
+import java.net.URLClassLoader
 import java.util.Locale
 import java.util.ResourceBundle
 
-class TranslationsProvider {
+class TranslationsProvider(private val plugin: TPA) {
 
     private val key = Key.key("de.l4zs.tpa")
 
-    private val translationRegistry = TranslationRegistry.create(key).apply {
-        defaultLocale(Locale.US)
+    private var translationRegistry = TranslationRegistry.create(key).apply {
+        defaultLocale(Locale.ENGLISH)
     }
 
-    private val locales = listOf(Locale.US, Locale.GERMANY)
+    val locales = mutableListOf<Locale>()
 
     fun setDefault(locale: Locale) {
         translationRegistry.defaultLocale(locale)
     }
 
-    fun registerTranslations() {
-        val bundleNames = mutableListOf(
-            "translations.general",
-        )
-
-        locales.forEach { locale ->
-            bundleNames.forEach { bundleName ->
-                translationRegistry.registerAll(
-                    locale,
-                    ResourceBundle.getBundle(bundleName, locale),
-                    false
-                )
+    fun unregisterTranslations(defaultLocale: Locale) {
+        if (GlobalTranslator.translator().sources().contains(translationRegistry)) {
+            GlobalTranslator.translator().removeSource(translationRegistry)
+            translationRegistry = TranslationRegistry.create(key).apply {
+                defaultLocale(defaultLocale)
             }
+        }
+    }
+
+    fun registerTranslations() {
+        locales.forEach {
+            translationRegistry.registerAll(
+                it,
+                resourceBundleFromClassLoader(plugin.dataFolder.resolve("translations").path, "general", it),
+                false
+            )
         }
         GlobalTranslator.translator().addSource(translationRegistry)
     }
+}
+
+@Throws(MalformedURLException::class)
+private fun resourceBundleFromClassLoader(dir: String, bundleName: String, locale: Locale): ResourceBundle {
+    val file = File(dir)
+    val urls = arrayOf(file.toURI().toURL())
+    val loader: ClassLoader = URLClassLoader(urls)
+    return ResourceBundle.getBundle(bundleName, locale, loader)
 }
